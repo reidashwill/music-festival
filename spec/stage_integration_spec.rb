8ae64fb9038fbec2022168528dabbbba086ab1e3
Reid Ashwill <reidashwill@gmail.com>
2020-05-13 16:07:18 -0700

    add pass test for artist path

--- spec/stage_integration_spec.rb
+++ spec/stage_integration_spec.rb
@@ -12,3 +12,14 @@ describe('create a stage path', {:type => :feature}) do
     expect(page).to have_content('west stage')
   end
 end
+
+describe('create an artist path', {:type => :feature}) do
+  it('creates an artist and then goes to the artist page') do
+    stage = Stage.new("Best stage ever", nil)
+    stage.save
+    visit("/stages/#{stage.id}")
+    fill_in('artist_name', :with => 'best band ever')
+    click_on('Add Artist')
+    expect(page).to have_content("best band ever")
+  end
+end
