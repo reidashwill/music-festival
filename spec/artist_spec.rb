2870f32439372ee01e75b036cb3f9273fff80dc6
Reid Ashwill <reidashwill@gmail.com>
2020-05-13 13:26:18 -0700

    passing test for all method

---
+++ spec/artist_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require 'artist'
+require 'festival'
+require 'pry'
+
+describe '#Artist' do
+  
+  before(:each) do
+    Stage.clear()
+    Artist.clear()
+    @stage =Stage.new("north stage", nil)
+    @stage.save()
+  end
+
+  describe('#==') do
+    it("is the same artist if it has the same attributes as another artist") do
+      artist = Artist.new("Gwar", @stage.id, nil)
+      artist2 = Artist.new("Gwar", @stage.id, nil)
+      expect(artist).to(eq(artist2))
+    end
+  end
+
+  describe('.all') do
+    it("return a list of all artists") do
+      artist = Artist.new("Gwar", @stage.id, nil)
+      artist.save()
+      artist2 = Artist.new("The Baby Lottery", @stage.id, nil)
+      artist2.save()
+      expect(Artist.all).to(eq([artist,artist2]))
+    end
+  end
+end
